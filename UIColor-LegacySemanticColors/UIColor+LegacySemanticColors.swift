a156ade2f118be34a5fb96feb5b60d8bfe702591
Tim Oliver <me@timoliver.com.au>
2020-04-26 15:42:37 +0800

    Added Swift extension file

---
+++ UIColor-LegacySemanticColors/UIColor+LegacySemanticColors.swift
@@ -0,0 +1,11 @@
+//
+//  UIColor+LegacySemanticColors.swift
+//  LegacySemanticColors
+//
+//  Created by Tim Oliver on 26/4/20.
+//  Copyright © 2020 Tim Oliver. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor
